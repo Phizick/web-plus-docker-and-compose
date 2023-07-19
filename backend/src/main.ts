3c2bee729b1064b5d1b856ebdd35a9b7411aba1b
Denis Kraev <deniskraevcc@gmail.com>
2023-07-19 19:48:34 +0300

    танцы с бубном вокруг корса)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: false });
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors({
-    origin: 'https://kraev.nomoredomains.xyz'
+    origin: 'https://kraev.nomoredomains.xyz',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true,
   });
   await app.listen(3000);
   console.log(`running on: ${await app.getUrl()}`);
