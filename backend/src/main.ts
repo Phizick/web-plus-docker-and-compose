8b435189449cd36a5a657d438c277e11e86cc632
Denis Kraev <deniskraevcc@gmail.com>
2023-07-19 16:12:13 +0300

    port fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import config from '../config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors();
-  await app.listen(3001);
+  await app.listen(config().port);
   console.log(`running on: ${await app.getUrl()}`);
 }
 bootstrap();
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,10 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
+import { CacheModule } from '@nestjs/common';
+import cacheConfig from '../config/cache.config';
+
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  exports: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    CacheModule.register(cacheConfig),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
