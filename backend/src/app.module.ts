eb8f8a23d20810204684eff3c43e20361c7e7ff5
Denis Kraev <deniskraevcc@gmail.com>
2023-07-19 14:07:42 +0300

    added db config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import config from '../config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/entities/user.entity';
 import { Wish } from './wishes/entities/wish.entity';
@@ -21,22 +22,18 @@ const modules = [
 ];
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DATABASE_HOST'),
-        port: configService.get('DATABASE_PORT'),
-        username: configService.get('DATABASE_USERNAME'),
-        password: configService.get('DATABASE_PASSWORD'),
-        database: configService.get('DATABASE_NAME'),
-        entities,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+    ConfigModule.forRoot({ load: [config] }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: config().database.host,
+      port: config().database.port,
+      username: config().database.username,
+      password: config().database.password,
+      database: config().database.database,
+      entities: entities,
+      synchronize: config().database.synchronize,
     }),
-    ConfigModule.forRoot({ isGlobal: true }),
-    ...modules,
+    modules,
   ],
 })
 export class AppModule {}
